589b362b011eb399dddcedd20bbccaf6d0ee1d55
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-10-30 20:19:35 +0300

    Adding products, product details, contact us, improving homepage

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import contactUs, index, productDetails, products
 
-urlpatterns = [path("", index, name="index")]
+urlpatterns = [
+    path("", index, name="index"),
+    path("products", products, name="products"),
+    path("product-details", productDetails, name="product-details"),
+    path("contact-us", contactUs, name="contact-us"),
+]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def products(request):
+    return render(request, "products.html")
+
+def productDetails(request):
+    return render(request, "product_details.html")
+
+def contactUs(request):
+    return render(request, "contact_us.html")
