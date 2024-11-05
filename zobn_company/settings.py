b40e05f42c4016d28dbf963d73cedc8780926fcb
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-11-05 21:29:15 +0300

    Adding more formating for the price

--- zobn_company/settings.py
+++ zobn_company/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "compressor",
-    'core'
+    'core',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
